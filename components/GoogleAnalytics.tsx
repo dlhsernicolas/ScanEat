b7f6f3be11c4411b4370423b69608cbfe3648687
dlhsernicolas <dlhsernicolas@gmail.com>
2025-06-12 21:13:46 -0300

    a

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,23 +2,29 @@ import React, { useEffect } from 'react';
 
 const GA_MEASUREMENT_ID = 'G-Y8NKT8RHB8';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
 const GoogleAnalytics = () => {
   useEffect(() => {
-    // Cargar script gtag.js
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
-    script.async = true;
-    document.head.appendChild(script);
-
-    // Inicializar gtag
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]) {
-      window.dataLayer.push(args);
+    if (!document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}"]`)) {
+      const script = document.createElement('script');
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+      script.async = true;
+      document.head.appendChild(script);
     }
-    window.gtag = gtag;
 
-    gtag('js', new Date());
-    gtag('config', GA_MEASUREMENT_ID);
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function(...args: any[]) {
+      window.dataLayer.push(args);
+    };
+
+    window.gtag('js', new Date());
+    window.gtag('config', GA_MEASUREMENT_ID);
   }, []);
 
   return null;
