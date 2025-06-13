fedb764f411d8ce6aa22a4b7b04e3e0b05f44248
dlhsernicolas <dlhsernicolas@gmail.com>
2025-06-12 21:18:07 -0300

    a

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,29 +2,28 @@ import React, { useEffect } from 'react';
 
 const GA_MEASUREMENT_ID = 'G-Y8NKT8RHB8';
 
-declare global {
-  interface Window {
-    dataLayer: any[];
-    gtag: (...args: any[]) => void;
-  }
-}
-
 const GoogleAnalytics = () => {
   useEffect(() => {
-    if (!document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}"]`)) {
+    const scriptTagId = 'ga-gtag';
+
+    // Si el script ya existe, no lo volvemos a cargar
+    if (!document.getElementById(scriptTagId)) {
       const script = document.createElement('script');
       script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
       script.async = true;
+      script.id = scriptTagId;
+      script.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(...args) {
+          window.dataLayer.push(args);
+        }
+        window.gtag = gtag;
+
+        window.gtag('js', new Date());
+        window.gtag('config', GA_MEASUREMENT_ID);
+      };
       document.head.appendChild(script);
     }
-
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function(...args: any[]) {
-      window.dataLayer.push(args);
-    };
-
-    window.gtag('js', new Date());
-    window.gtag('config', GA_MEASUREMENT_ID);
   }, []);
 
   return null;
